79685e464671142ff5d82451cebd6bb835a3cea8
yoonsuhchung <lucy1013@snu.ac.kr>
2022-11-26 02:22:07 +0900

    printtime amend

--- waffle/management/commands/printtime.py
+++ waffle/management/commands/printtime.py
@@ -1,9 +1,9 @@
 from django.core.management.base import BaseCommand
-from django.utils import timezone
+from datetime import datetime
 
 
 class Command(BaseCommand):
     help = '현재 시간 프린트'
 
     def handle(self, *args, **options):
-        self.stdout.write(self.style.SUCCESS('The time now is %s' % timezone.now))
+        self.stdout.write(self.style.SUCCESS('The time now is %s' % datetime.now().strftime("%Y-%m-%d %H:%M:%S %Z")))
